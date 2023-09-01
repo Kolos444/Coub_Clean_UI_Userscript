b3dd4f2fb0baa16a258420b843790642ae544418
Luca Sonntag <luca.sonntag@online.de>
2023-09-01 22:51:54 +0200

    Somewhat usable release

--- main.js
+++ main.js
@@ -16,7 +16,11 @@
 
 	const btn = document.createElement("button");
 	btn.innerHTML = "Clean";
+	btn.classList.add("clean-ui-button");
 	const style = document.createElement("style");
+	const btnStyle = document.createElement("style");
+	btnStyle.innerHTML = ".clean-ui-button{z-index: 9999;position:fixed;bottom:50px;left: 20px;aspect-ratio: 1;border-radius: 50%;border: 2px white;background: darkred;color: white;}"
+	btnStyle.innerHTML += "*{overflow-anchor: none;}"
 
 	/*Triggered after the Site is fully loaded.
 	* Avoids having Trouble when accessing the DOM.*/
@@ -24,7 +28,10 @@
 
 		const body = document.querySelector(".page__content");
 		body.insertBefore(btn, body.firstChild);
-		insert.insertBefore(style, insert.firstChild);
+
+		const head = document.querySelector("head");
+		head.insertBefore(style, head.firstChild);
+		head.insertBefore(btnStyle, head.firstChild);
 
 		assignBtnClick();
 	}, false);
@@ -42,12 +49,12 @@
 	};
 
 
-
 	const cleanHomePage = () => {
 		// language=CSS
-		style.innerHTML = ".coub--timeline,.viewer,.page-container{ width: unset !important; height: unset !important;}";
-		style.innerHTML += ".page-menu,.main-menu__inner,.header,.timeline-right-block{display: none !important;}";
-		style.innerHTML += ".page__content{width: 80% !important;}";
+		//style.innerHTML = ".coub--timeline,.viewer,.page-container{ width: unset !important; height: unset !important;}";
+		style.innerHTML = ".page-menu,.main-menu__inner,.header,.timeline-right-block{display: none !important;}";
+		style.innerHTML += ".coub--timeline{width:unset !important;}";
+		//style.innerHTML += ".page__content{width: 80% !important;}";
 		const insert = document.querySelector("head");
 
 		btn.onclick = clearStyle;
